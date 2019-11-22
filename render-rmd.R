bad8ba80f74bfd60ded8d041062770ad5f19c6ba
unknown <GLS-TAGR8324@AD.WISC.EDU>
2019-11-22 11:31:22 -0600

    added error message for no files in directory

--- render-rmd.R
+++ render-rmd.R
@@ -6,6 +6,10 @@ file_names <- commandArgs(trailingOnly = TRUE)
 if("-a" %in% file_names){
   folder<-file_names[2]
   file_names<-Sys.glob(str_c(folder, "/*.csv"))
+  
+  if(length(file_names) == 0){
+    print("No files found in the specified folder.")
+  }
 } else {file_names<-file_names}
 
 
