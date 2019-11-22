86a240d59ad79d42c3d404f27948daf18827520c
unknown <GLS-TAGR8324@AD.WISC.EDU>
2019-11-22 11:28:07 -0600

    introduced the -a flag

--- render-rmd.R
+++ render-rmd.R
@@ -3,6 +3,12 @@ library("stringr")
 
 file_names <- commandArgs(trailingOnly = TRUE)
 
+if("-a" %in% file_names){
+  folder<-file_names[2]
+  file_names<-Sys.glob(str_c(folder, "/*.csv"))
+} else {file_names<-file_names}
+
+
 for(file in file_names)
 {
   out_name <- str_split_fixed(string = file, 
